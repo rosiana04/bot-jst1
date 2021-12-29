19b64a5f8d1e92a3e0072fb2ad258f273a6096dd
rosiana04 <96753665+rosiana04@users.noreply.github.com>
2021-12-29 13:56:08 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '5076601643:AAFJpsXDYyiEjpq73BBS7dApHOJHPGWSKBo'
 const bot = new TelegramBot(token, {polling: true});
 
 
